b9ccba91328317a38761069197f5edb0cf8ddc05
marek <marek1990kaminski@gmail.com>
2017-08-23 10:30:39 +0200

    now the whole sass-watch-with-plumber thing is working

---
+++ config.rb
@@ -0,0 +1,14 @@
+#require 'susy'
+#require 'breakpoint'
+
+project_type = :stand_alone
+http_path = "/"
+project = "assets"
+sass_dir = "scss"
+css_dir = "css"
+images_dir = "img"
+javascript_dir = "js"
+line_comments = false
+preferred_syntax = :scss
+output_style = :compressed
+relative_assets = true
--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,34 @@
 
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    sass = require('gulp-ruby-sass');
+    sass = require('gulp-ruby-sass'),
+    plumber = require('gulp-plumber');
 
 
 //scripts task
+//uglify
 gulp.task('scripts', function () {
     gulp.src('assets/js_src/**/*.js')
+        .pipe(plumber())
         .pipe(uglify())
         .pipe(gulp.dest('assets/js_dest'));
 });
 
-//styles task
-//uglifies
-gulp.task('styles', function () {
-    gulp.src('assets/css/style2.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('assets/css/'));
-});
+//compass task
+gulp.task('sass', function () {
+    return sass('assets/scss/*.scss', {style: 'compressed'})
+        .on('error', sass.logError)
+        .pipe(plumber())
+        .pipe(gulp.dest('assets/css'));
+});﻿
+
+
 
 //watch task
 gulp.task('watch', function () {
     gulp.watch('assets/js_src/**/*.js', ['scripts']);
+    gulp.watch('assets/scss/*.scss', ['sass']);
 });
 
 //default
-gulp.task('default', ['scripts', 'styles', 'watch']);
+gulp.task('default', ['scripts', 'sass', 'watch']);
